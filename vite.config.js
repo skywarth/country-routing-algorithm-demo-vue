5102f7d7499fbcffe010a258a97eb305e8744d8c
skywarth <yigitk.ersoy@gmail.com>
2023-05-14 00:10:28 +0300

    feat: add base publicPath for github pages deploy

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'/country-routing-algorithm-demo-vue/',//TODO check node_env: process.env.NODE_ENV === "production"
   plugins: [vue()],
   resolve: {
     alias: {
