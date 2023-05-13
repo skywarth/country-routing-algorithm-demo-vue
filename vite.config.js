64e415051fd90595a04fdcc3d31c9ed062587f96
skywarth <yigitk.ersoy@gmail.com>
2023-05-14 00:56:38 +0300

    test commit for deploy

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   base:'/country-routing-algorithm-demo-vue/',//TODO check node_env: process.env.NODE_ENV === "production"
+  //uhh why base path not working ?
   plugins: [vue()],
   resolve: {
     alias: {
