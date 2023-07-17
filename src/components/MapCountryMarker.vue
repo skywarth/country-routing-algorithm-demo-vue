2f651f0811c164a8634232b658e654765137be75
skywarth <yigitk.ersoy@gmail.com>
2023-07-17 23:29:35 +0300

    feat: additional details on popup

--- src/components/MapCountryMarker.vue
+++ src/components/MapCountryMarker.vue
@@ -1,11 +1,14 @@
 <template>
   <l-marker  :lat-lng="countryCenterLatLng" >
     <!-- Didn't use the foundPathLatLngs because we'll need the attributes for the popup -->
-    <l-popup>
+    <l-popup :options="{maxWidth:700}">
         <div>
-          <h3 class="text-center">{{this.country.attributes.name.common}}</h3>
-          <p>hey</p>
-          <p>wazzup?</p>
+          <p class="text-center mb-0">{{this.flag}}</p>
+          <h3 class="text-center mt-0">{{this.country.attributes.name.common}} ({{this.country.countryCode}})</h3>
+          <p>LatLng: {{this.countryCenterLatLng}}</p>
+          <p>Distance to Destination: {{this.distanceToFinalDestination}} KM </p>
+          <p>Distance between Node: {{this.distanceBetweenNode}} KM </p>
+
         </div>
     </l-popup>
   </l-marker>
@@ -13,6 +16,8 @@
 
 <script>
 import {LMarker,LPopup} from "@vue-leaflet/vue-leaflet";
+
+import CRA from "country-routing-algorithm";
 export default {
   name: "MapCountryMarker",
   components: {
@@ -20,11 +25,21 @@ export default {
     LPopup
   },
   props: {
-    country:Object
+    country:CRA.TraverseCountryNode
   },
   computed:{
     countryCenterLatLng(){
       return this.country.attributes.latlng;
+    },
+    distanceToFinalDestination(){
+      return Math.round(this.country.distanceToFinalDestination);
+    },
+    distanceBetweenNode(){
+      return Math.round(this.country.distanceBetweenNode);
+    },
+
+    flag(){
+      return this.country.attributes.flag
     }
 
   },
