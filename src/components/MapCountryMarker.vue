ee05dca73cdf38d2064ef20aa2a9bb2d644d6331
skywarth <yigitk.ersoy@gmail.com>
2023-07-18 00:49:44 +0300

    feat: marker icon

--- src/components/MapCountryMarker.vue
+++ src/components/MapCountryMarker.vue
@@ -1,6 +1,7 @@
 <template>
   <l-marker  :lat-lng="countryCenterLatLng" >
     <!-- Didn't use the foundPathLatLngs because we'll need the attributes for the popup -->
+    <l-icon :icon-url="iconUrl" :icon-size="iconSize" />
     <l-popup :options="{maxWidth:700}">
         <div>
           <p class="text-center mb-0">{{this.flag}}</p>
@@ -15,19 +16,36 @@
 </template>
 
 <script>
-import {LMarker,LPopup} from "@vue-leaflet/vue-leaflet";
+import {LMarker,LPopup,LIcon} from "@vue-leaflet/vue-leaflet";
+import marker from '@/assets/marker2.png' // Import the marker image
+
 
 import CRA from "country-routing-algorithm";
 export default {
   name: "MapCountryMarker",
   components: {
+    LIcon,
     LMarker,
     LPopup
   },
   props: {
     country:CRA.TraverseCountryNode
   },
+  data(){
+    return{
+
+    }
+  },
   computed:{
+    iconUrl() {
+      return marker;
+    },
+    iconSize(){
+      return [24, 24];
+    },
+
+
+
     countryCenterLatLng(){
       return this.country.attributes.latlng;
     },
